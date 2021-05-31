1e94fc87c12268c59cffa16647f80b49c0a40fa4
SamSanket9595 <84986110+SamSanket9595@users.noreply.github.com>
2021-05-31 18:58:15 +0530

    Update PrintPattern1.cpp

--- PrintPattern1.cpp
+++ PrintPattern1.cpp
@@ -2,8 +2,8 @@
 //	Q. Accept two numbers and display first number 
 //	   in second number of times.
 //
-//	Input:	12	5	|	-2  0      | -2   3   
-//  	Output: 12 12 12 12 12  |   		   | -2  -2  -2
+//	Input:	12	5	|	-2  0      | 2   -3   
+//  	Output: 12 12 12 12 12  |   		   | 2  2  2
 //
 //	
 //	Input: 		Int,Int

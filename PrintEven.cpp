b347772274a3f4e627a6b945b0a30182c994b0d3
SamSanket9595 <84986110+SamSanket9595@users.noreply.github.com>
2021-06-01 19:39:30 +0530

    Add files via upload

---
+++ PrintEven.cpp
@@ -0,0 +1,56 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+//	Q.	Write a program which accept one number from
+//		user and print that number of even number
+//		on the screen.
+//		
+//		Input:  4		|	6				|	7
+//		Otput: 2 4 6 8	| 2 4 6 8 10 12		| 2 4 6 8 10 12 14
+//	
+//		Technology: C++
+//		Input: Int
+//		Output: Int
+//	
+//		Author: Sanket Kolhe
+//		Date : 01/06/2021
+//	
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include<iostream>
+using namespace std;
+
+
+class PrintEven
+{
+	public:
+		void evenNumbers(int);
+};
+
+
+void PrintEven :: evenNumbers(int n)
+{
+	int i=0;
+	
+	for(i=1;i<=n;i++)
+	{
+		cout<<i*2<<' ';
+	}
+	
+}
+
+
+int main()
+{
+	int n=0;
+	
+	cout<<"Enter how many even numbers ";
+	cin>>n;
+	
+	PrintEven obj;
+	
+	obj.evenNumbers(n);
+	
+	return 0;
+}

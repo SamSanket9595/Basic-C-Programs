403f1d904bbbe992f9623786432d59f977349a87
SamSanket9595 <84986110+SamSanket9595@users.noreply.github.com>
2021-06-03 17:22:53 +0530

    Add files via upload

---
+++ Pattern.cpp
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////
+//	Q. Write a program which accept number from user and
+//		print that number of $ and * on the screen.
+//		
+//	Input:  5					 |     3			|  -3
+//	Output: $ * $ * $ * $ * $ *  | $ * $ * $  * 	| $ * $ * $ *
+//	
+//	
+//	Technology: C++
+//	Input: Int
+//	Output: Char
+//	
+//	Author: Sanket Kolhe
+//	Date : 03/06/2021
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+using namespace std;
+
+
+class Pattern
+{
+	public: 
+		void printPattern(int);
+};
+
+
+void Pattern :: printPattern(int no)
+{
+	int i=0;
+	
+	if(no<0)//Updater
+	{
+		no= -no;
+	}
+	
+	
+	for(i=1;i<=no;i++)
+	{
+		cout<<"$  *";
+		cout<<" ";
+	}
+}
+
+
+
+int main()
+{
+	int no=0;
+	
+	cout<<"Enter the number ";
+	cin>>no;
+	
+	Pattern obj;
+	obj.printPattern(no);
+	
+	
+	return 0;
+}
